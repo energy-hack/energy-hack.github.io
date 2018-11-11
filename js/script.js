7415c8f62ff8cafbc2061a91ec1aff6fbd9c30fa
Aleksey Bykhun <theisease@gmail.com>
2018-11-11 05:23:00 +0300

    order form: extract data, update, submit

--- js/script.js
+++ js/script.js
@@ -11,22 +11,36 @@ window.addEventListener('load', async () => {
 
   $('.token_balance').attr("href", "https://rinkeby.etherscan.io/address/" + tokenAddress)
 
+  window.buyStarterPack = () => {
+    const { current_load, promised_load, percent } = updateOrderForm()
+
+    console.log(`Deploy contract with params ${ { current_load, promised_load, percent } }`)
+    // .........
+    //
+    // const params = { ... }
+    // const schneider_contract = Contract.deploy(web3, _SCHNEIDER_BIN, _SCHNEIDER_ABI, ...params)
+    //
+    // const token = noxonToken
+    // token.transfer(wallet.account.address, contract.address, )
+    // new Contract(...)
+    // wallet.deploy contract
+  }
+
   window.updateBalance = async () => {
     const wei = await wallet.getBalance()
     const balance = web3.utils.fromWei(wei)
-  
+
     const tokenBalance = await wallet.getTokenBalance(noxonToken)
-  
+
     console.log(`updateBalance() ---> Balance: ${Number(balance).toFixed(3)}`)
     console.log(`updateBalance() ---> NOXON Balance: ${Number(tokenBalance).toFixed(3)}`)
-  
+
     const noxonTokenName = await noxonToken.getSymbol()
-  
+
     $('.token_balance').text( Number(tokenBalance).toFixed(2) + ' ' + noxonTokenName)
 
     return true
   }
 
   window.updateBalance()
-  
 });
